b7a490e205f812a180ac3f770defc4fd0cd5ecf6
codewthr <rahulbhatia.ramm@gmail.com>
2024-12-18 23:01:50 +0530

    PYchache

--- ecommerce/ecomm/views.py
+++ ecommerce/ecomm/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from ecomm import models
 from .models import product
 
 # Create your views here.
-def home (request):
-    return render(request, 'index.html')
+# def home (request):
+#     return render(request, 'index.html')
 
 
 def product(request):
+    print(product)
     products = product.objects.all()
     return render(request , 'index.html', { 'product': product})
+ 
+
