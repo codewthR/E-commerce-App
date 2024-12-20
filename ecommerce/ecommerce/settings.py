b88f3cd92b0e4514f598045576f8921beaab0f6c
codewthr <rahulbhatia.ramm@gmail.com>
2024-12-20 23:11:09 +0530

    Media changes

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'  # URL to access media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
